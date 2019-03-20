3c83b7723502672f8e2c6868986b920dd663ec4d
Chayan Datta <sonicxxx7@gmail.com>
2019-03-20 12:45:32 +0530

    Formspree added

--- newsfeed.php
+++ newsfeed.php
@@ -7,7 +7,7 @@ if (isset($_POST['submit'])) {
     $message = $first_name . " "  . " wrote the following:" . "\n\n";
    $headers = "From: Contact Form <info@thediscretelabs.com>" . "\r\n" .
 			"Reply-To: $first_name" . "\r\n" .
-			"MIME-Version: 1.0\r\n" . 
+			"MIME-Version: 1.0\r\n" .
 			"Content-Type: text/html; charset=iso-8859-1\n";
     if(mail($to, $subject, $message, $headers))
     {
