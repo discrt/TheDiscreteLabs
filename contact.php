9ad2e27d6390002ec332d9476a1df34f34d76b7f
Chayan Datta <sonicxxx7@gmail.com>
2019-03-20 12:40:51 +0530

    Mail figured out

--- contact.php
+++ contact.php
@@ -5,12 +5,36 @@ if (isset($_POST['submit'])) {
     $first_name = $_POST['name'];
     $subject = $_POST['subject'];
     $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+    $message = $_POST['message'];
+    $emailBody = "
+	<html>
+	<head>
+		<title>$subject</title>
+	</head>
+	<body style=\"background-color:#fafafa;\">
+		<div style=\"padding:20px;\">
+			Name: <span style=\"color:#888\">$first_name</span>
+			<br>
+			Email: <span style=\"color:#888\">$from</span>
+			<br>
+			Message: <div style=\"color:#888\">$message</div>
+		</div>
+	</body>
+	</html>
+";
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $message;
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    if(mail($to, $subject, $message, $headers))
+    $headers = "From: Contact Form <info@thediscretelabs.cm>" . "\r\n" .
+        "Reply-To: $from" . "\r\n" .
+        "MIME-Version: 1.0\r\n" .
+        "Content-Type: text/html; charset=iso-8859-1\n";
+
+    $headers2 = "From: Contact Form <info@thediscretelabs.cm>" . "\r\n" .
+        "Reply-To: $to" . "\r\n" .
+        "MIME-Version: 1.0\r\n" .
+        "Content-Type: text/html; charset=iso-8859-1\n";
+
+    if(mail($to, $subject, $emailBody, $headers))
     {
         header('Location: index.html');
     } else {
